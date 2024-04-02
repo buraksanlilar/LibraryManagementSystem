717226f0037e63ab47f367734340852c1879cb80
sucreistaken <kadiraytarsus33@gmail.com>
2024-04-02 21:27:07 +0300

    startup

--- src/main/java/com/example/hellofx1/App.java
+++ src/main/java/com/example/hellofx1/App.java
@@ -3,68 +3,110 @@ package com.example.hellofx1;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+
+import java.io.*;
+import java.nio.file.Files;
 
 public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-    private Label label;
-    private TextField txtInfo;
-    private ListView<String> listView;
-    private Button bttnAdd, bttnOK;
-    private void addAction() {
-        if(txtInfo.getText().isBlank()) return;
-        listView.getItems().add(txtInfo.getText());
-        txtInfo.clear();
+    private void newFile(TextArea textArea)
+    {
+        textArea.clear();
+    }
+    private void openFile(Stage stage,TextArea textArea)
+    {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Select file to open!");
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("txt","*.txt"));
+        File f = fc.showOpenDialog(stage);
+       if(f != null)
+       {
+           try {
+              String content = Files.readString(f.toPath());
+              textArea.setText(content);
+           }catch (IOException e){
+               e.printStackTrace();
+           }
+       }
+    }
+    private void saveFile(Stage stage,TextArea textArea)
+    {
+
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Select file to save!");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT","*.txt"));
+        File f = fc.showSaveDialog(stage);
+
+        if(f != null)
+        {
+            try {
+                Files.writeString(f.toPath(),textArea.getText());
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void helpDisplay()
+    {
+       Alert alert = new Alert(Alert.AlertType.INFORMATION);
+       alert.setTitle("Information !!!");
+       alert.setHeaderText("about 2024");
+       alert.showAndWait();
     }
 
-//denens
     @Override
     public void start(Stage stage) throws Exception {
         VBox mainLayout = new VBox();
+        TextArea textArea = new TextArea();
+        VBox.setVgrow(textArea,Priority.ALWAYS);
 
-        HBox firstLine = new HBox(8); // widgetların arasındaki mesafeyi ayarlıyor
-        firstLine.setAlignment(Pos.CENTER);
-        label = new Label("Book name: ");
-        txtInfo = new TextField();
-        HBox.setHgrow(txtInfo, Priority.ALWAYS);
-        bttnAdd = new Button("Add");
-        bttnAdd.setOnAction(e -> addAction());
 
-        listView = new ListView<String>();
-        VBox.setVgrow(listView, Priority.ALWAYS);
+        MenuBar menuBar = new MenuBar();
 
-        HBox lastLine = new HBox(8);
-        lastLine.setAlignment(Pos.CENTER_RIGHT);
-        bttnOK = new Button("OK");
-        bttnOK.setOnAction(e -> stage.close());
-        lastLine.getChildren().add(bttnOK);
+        Menu mFile = new Menu("File");
+        Menu mHelp = new Menu("Help");
 
-        VBox.setMargin(firstLine, new Insets(8));
-        VBox.setMargin(listView, new Insets(8));
-        VBox.setMargin(lastLine, new Insets(8));
-        // dış duvarla arasındaki mesafeyi ayarlıyor
-        //velevki
-        firstLine.getChildren().addAll(
-                label, txtInfo, bttnAdd
-        );
+        MenuItem myNewFile = new MenuItem("New");
+        myNewFile.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+        myNewFile.setOnAction(e->newFile(textArea));
 
-        mainLayout.getChildren().addAll(firstLine,
-                listView, lastLine);
+        MenuItem myOpen = new MenuItem("Open");
+        myOpen.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+        myOpen.setOnAction( e-> openFile(stage,textArea));
+
+        MenuItem mySave = new MenuItem("Save");
+        mySave.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+        mySave.setOnAction(e->saveFile(stage,textArea));
+
+       MenuItem About = new MenuItem("About");
+       About.setOnAction(e->helpDisplay());
+
+       mHelp.getItems().add(About);
+
+
+        mFile.getItems().addAll(myNewFile,myOpen,mySave);
+
+        menuBar.getMenus().addAll(mFile,mHelp);
+        mainLayout.getChildren().addAll(menuBar,textArea);
+
+
+
+
 
         Scene scene = new Scene(mainLayout, 400,
                 300);
-        String burak = "kanka valla deniyom";
         stage.setTitle("Book Catalog");
         stage.setScene(scene);
         stage.show();
