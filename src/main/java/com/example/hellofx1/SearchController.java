759b1dd0726004e515a3297bc3b9b37cd0a5e7e8
buraksanlilar <120301435+buraksanlilar@users.noreply.github.com>
2024-04-10 01:01:26 +0300

    son

--- src/main/java/com/example/hellofx1/SearchController.java
+++ src/main/java/com/example/hellofx1/SearchController.java
@@ -1,4 +1,71 @@
 package com.example.hellofx1;
 
+import com.example.hellofx1.Book;
+import com.example.hellofx1.Library;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import java.util.ArrayList;
+
 public class SearchController {
+    @FXML
+    private TextField title;
+    @FXML
+    private TextField subtitle;
+    @FXML
+    private TextField isbn;
+    @FXML
+    private TextField authors;
+    @FXML
+    private TextField translator;
+    @FXML
+    private TextField publisher;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private TextField covertype;
+    @FXML
+    private TextField edition;
+    @FXML
+    private TextField page;
+    @FXML
+    private TextField tag;
+
+
+
+
+    private ArrayList<Book> searchBooks() {
+        String titleSearch = title.getText().toLowerCase();
+        String subtitleSearch = subtitle.getText().toLowerCase();
+        String isbnSearch = isbn.getText();
+        String authorsSearch = authors.getText().toLowerCase();
+        String translatorSearch = translator.getText().toLowerCase();
+        String publisherSearch = publisher.getText().toLowerCase();
+        String dateSearch = String.valueOf(date.getValue());
+        String coverTypeSearch = covertype.getText().toLowerCase();
+        String editionSearch = edition.getText();
+        String pageSearch = page.getText();
+        String tagSearch = tag.getText().toLowerCase();
+
+        ArrayList<Book> results = new ArrayList<>();
+
+
+        for (Book book : Library.books) {
+            if ((titleSearch.isEmpty() || book.getTitle().toLowerCase().contains(titleSearch)) &&
+                    (subtitleSearch.isEmpty() || book.getSubTitle().toLowerCase().contains(subtitleSearch)) &&
+                    (isbnSearch.isEmpty() || book.getIsbn().equals(isbnSearch)) &&
+                    (authorsSearch.isEmpty() || book.getAuthors().contains(authorsSearch)) &&
+                    (translatorSearch.isEmpty() || book.getTranslators().contains(translatorSearch)) &&
+                    (publisherSearch.isEmpty() || book.getPublisher().toLowerCase().contains(publisherSearch)) &&
+                    (dateSearch.isEmpty() || book.getDate().equals(dateSearch)) &&
+                    (coverTypeSearch.isEmpty() || book.getCoverType().toLowerCase().contains( coverTypeSearch)) &&
+                    (editionSearch.isEmpty() || book.getEdition().equals(editionSearch)) &&
+                    (pageSearch.isEmpty() || book.getPage() == Integer.parseInt((pageSearch))) &&
+                    (tagSearch.isEmpty() || book.getTags().contains(tagSearch))) {
+                results.add(book);
+            }
+        }
+
+        return results;
+    }
 }
